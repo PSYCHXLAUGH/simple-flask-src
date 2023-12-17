a206d74caba8ac6c805cc711320549908518c427
psychx <#_#>
2023-12-17 23:11:47 +0300

    bug fix

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("some_feauture")
+    print("some_whatever")
+
+    print("yeeees")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
